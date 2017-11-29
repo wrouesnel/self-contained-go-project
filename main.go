7db55d163548a7e6e68facb3bc58609eb3d6c85e
Will Rouesnel <w.rouesnel@gmail.com>
2017-11-30 00:22:54 +1100

    Add a sample main.go source file.

---
+++ main.go
@@ -0,0 +1,11 @@
+// Sample program for the self-contained build environment
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This sample program exists to test the build environment.")
+}
